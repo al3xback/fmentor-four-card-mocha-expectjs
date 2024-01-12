2db320783afe46c6cfdd74d425d189dbefe713f4
alex <al3xback@gmail.com>
2024-01-12 15:35:34 +0700

    fixed test wording

--- test/main.js
+++ test/main.js
@@ -28,24 +28,24 @@ describe('DOM', () => {
 	});
 
 	it('should have only one section element', () => {
-		const sectionElements = document.querySelectorAll('section');
+		const sectionEls = document.querySelectorAll('section');
 
-		expect(sectionElements.length).to.eql('1');
+		expect(sectionEls.length).to.eql('1');
 	});
 
 	it('should have a string type of card title inside card list element', () => {
-		const cardTitleElements = document.querySelectorAll('.card__title');
+		const cardTitleEls = document.querySelectorAll('.card__title');
 
-		for (let i = 0; i < cardTitleElements.length; i++) {
-			const cardTitle = cardTitleElements[i].textContent;
+		for (let i = 0; i < cardTitleEls.length; i++) {
+			const cardTitle = cardTitleEls[i].textContent;
 			expect(cardTitle).to.be.a('string');
 		}
 	});
 
 	it("should have a word 'Artificial Intelligence' in section description element", () => {
-		const sectionDescription =
+		const sectionDesc =
 			document.querySelector('.section__desc').textContent;
 
-		expect(sectionDescription).to.contain('Artificial Intelligence');
+		expect(sectionDesc).to.contain('Artificial Intelligence');
 	});
 });
